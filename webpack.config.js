3c58c133a70b2577d57a63d55b5909cd82ed6662
Matsvei Dubaleka <matvey.duboleko@yandex.ru>
2022-11-16 21:34:11 +0300

    refactor: add webpack config Wed, 16 Nov 2022 21:34:11 +0300

---
+++ webpack.config.js
@@ -0,0 +1,57 @@
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  context: path.resolve(__dirname, 'src'),
+  mode: 'production',
+  entry: './index.js',
+  output: {
+    filename: '[name].[contenthash].js',
+    clean: true,
+  },
+  optimization: {},
+  plugins: [
+    new HTMLWebpackPlugin({
+      title: 'Webpack Build',
+      template: './index.html',
+    }),
+    new CleanWebpackPlugin({
+      cleanOnceBeforeBuildPatterns: [path.join(__dirname, 'dist/**/*')],
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].[contenthash].css',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+      {
+        test: /\.s[ac]ss$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+      {
+        test: /\.(png|jpg|svg|gif)$/,
+        use: ['file-loader'],
+      },
+      {
+        test: /\.(ttf|woff|woff2|eot)$/,
+        use: ['file-loader'],
+      },
+    ],
+  },
+};
